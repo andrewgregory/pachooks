8338558aca45023b8ac2c549baca400540e92fc6
papajoker <patrickc538@gmail.com>
2016-02-17 13:51:20 +0100

    initial auto restart service

---
+++ scripts/serviced-restart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# auto restart service if running 
+[[ "${HTTP_USER_AGENT:0:6}" != 'pacman' ]] && exit 2
+
+while read -r service; do
+	service=${service##*\/}
+	if [[ $(systemctl show "${service}" 2>/dev/null | grep SubState) =~ "running" ]]; then
+		systemctl restart "${service}" && echo "${service} re-started"
+	fi
+done
+
+
