f670d8b59ba385646984a2805147c37014dc6d8d
papajoker <patrickc538@gmail.com>
2016-02-21 06:46:57 +0100

    only one daemon-reload

--- scripts/serviced-restart.sh
+++ scripts/serviced-restart.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # auto restart service if running 
 [[ "${HTTP_USER_AGENT:0:6}" != 'pacman' ]] && exit 2
-
+first=1
 while read -r service; do
-	service=${service##*\/}
-	if [[ $(systemctl show "${service}" 2>/dev/null | grep SubState) =~ "running" ]]; then
-		systemctl daemon-reload
-		systemctl restart "${service}" && echo "${service} re-started"
-	fi
+    service=${service##*\/}
+    if [[ $(systemctl show "${service}" 2>/dev/null | grep SubState) =~ "running" ]]; then
+        if (( first )); then
+            systemctl daemon-reload
+            unset first
+        fi
+        systemctl restart "${service}" && echo "${service} re-started"
+    fi
 done
-
-
